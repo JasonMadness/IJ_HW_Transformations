952ca8c8ac040eaee5907a5af73ea2c3537e0a7f
JasonMadness <tobc_madness@mail.ru>
2025-03-27 18:12:07 +0300

    Init commit

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,20 +3,9 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private bool _isSelfSpaced = false;
-
-    private Space _space;
-
-    private void Start()
-    {
-        if (_isSelfSpaced)
-            _space = Space.Self;
-        else
-            _space = Space.World;
-    }
 
     private void Update()
     {
-        transform.Translate(Vector3.forward * (_speed * Time.deltaTime), _space);
+        transform.Translate(Vector3.forward * (_speed * Time.deltaTime), Space.Self);
     }
 }
